1efe776fd72e31035caffc6ffd29ea6cb4548799
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-07-06 22:35:54 +0300

    Made function to get week ago date

--- github_rating.py
+++ github_rating.py
@@ -1,15 +1,22 @@
-from datetime import datetime, timedelta
 import requests
 
+from datetime import datetime, timedelta
+
+
 def get_week_ago_date():
+    raw_week_ago_datetime = (
+        datetime.now()
+        - timedelta(weeks=1)
+    )
+    return raw_week_ago_datetime.strftime("%d/%m")
 
 
 def find_popular_projects():
-
+    pass
 
 def main():
-
+    get_week_ago_date()
 
 if __name__ == '__main__':
-     main()
+    main()
 
