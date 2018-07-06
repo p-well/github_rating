fcf2b2ad9742d5238e6a01d8bb4bb2f87390e1dc
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-07-06 22:03:55 +0300

    Made script template

---
+++ github_rating.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+import requests
+
+def get_week_ago_date():
+
+
+def find_popular_projects():
+
+
+def main():
+
+
+if __name__ == '__main__':
+     main()
+
