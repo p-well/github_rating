311e83f2e91a686f546dae8b279925946abcbceb
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-07-06 22:37:25 +0300

    Minor update in get_week_ago_date func: year added

--- github_rating.py
+++ github_rating.py
@@ -8,14 +8,14 @@ def get_week_ago_date():
         datetime.now()
         - timedelta(weeks=1)
     )
-    return raw_week_ago_datetime.strftime("%d/%m")
+    return raw_week_ago_datetime.strftime('%Y-%m-%d')
 
 
 def find_popular_projects():
     pass
 
 def main():
-    get_week_ago_date()
+    print(get_week_ago_date())
 
 if __name__ == '__main__':
     main()
