99750950d844dc4aa2cd6a6a2ff645bc9644ba59
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-07-07 13:43:51 +0300

    Added func for fetch open issues

--- github_rating.py
+++ github_rating.py
@@ -11,24 +11,34 @@ def get_week_ago_date():
     return raw_week_ago_datetime.strftime('%Y-%m-%d')
 
 
-def find_popular_projects(week_ago_date, top_size):
+def fetch_popular_projects(week_ago_date, top_size):
     search_repo_url = 'https://api.github.com/search/repositories'
-    query_params = {
-        'language': 'python',
-        'q': 'created: > week_ago_date',
+    qualifiers = {
         'sort': 'stars',
         'order': 'desc',
+        'q':'created: > week_ago_date',
+        'language': 'python',
         'per_page': top_size
     }
     try:
-        raw_response = requests.get(search_repo_url, params=query_params)
-        #print(raw_response)
-        print(type(raw_response))
-        print(raw_response.json())
-        print(raw_response.json()['items'])
-        #return repo_response.json()['items']
+        raw_response = requests.get(search_repo_url, params=qualifiers)
+        most_popular_projects_data = raw_response.json().get('items')
+    except requests.exceptions.RequestException:
+        most_popular_projects_data = []
+    return most_popular_projects_data
+
+
+def fetch_open_issues_amount(most_popular_projects_data):
+    repo_full_name = most_popular_projects_data['full_name']
+    search_issues_url = (
+        'https://api.github.com/repos/{}/issues'.format(repo_full_name)
+    )
+    try:
+        issue_response = requests.get(search_issues_url)
+        return len(issue_response.json())
     except requests.exceptions.RequestException:
         return None
+
 
 def main():
     top_size = 2
@@ -36,6 +46,8 @@ def main():
     print(week_ago_date)
     find_popular_projects(week_ago_date, top_size)
 
+
 if __name__ == '__main__':
     main()
 
+# https://api.github.com/search/repositories?q=created:%3E2018-07-05+language:python&sort=stars&order=desc&per_page=20
