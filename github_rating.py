e7e4f10c113ac625a93093770319e30248b6d378
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-07-07 14:21:39 +0300

    Made results printing

--- github_rating.py
+++ github_rating.py
@@ -11,40 +11,76 @@ def get_week_ago_date():
     return raw_week_ago_datetime.strftime('%Y-%m-%d')
 
 
-def fetch_popular_projects(week_ago_date, top_size):
+def fetch_popular_projects(week_ago_date, top_repos_amount):
     search_repo_url = 'https://api.github.com/search/repositories'
     qualifiers = {
-        'sort': 'stars',
-        'order': 'desc',
-        'q':'created: > week_ago_date',
-        'language': 'python',
-        'per_page': top_size
+        'q': 'created:>{}'.format(week_ago_date),
+                        'language': 'python',
+                        'sort': 'stars',
+                        'order': 'desc',
+                        'per_page': top_repos_amount
+        # 'sort': 'stars',
+        # 'order': 'desc',
+        # 'q':'created: > week_ago_date',
+        # #'language': 'python',
+        # 'per_page': top_repos_amount
     }
     try:
         raw_response = requests.get(search_repo_url, params=qualifiers)
         most_popular_projects_data = raw_response.json().get('items')
     except requests.exceptions.RequestException:
         most_popular_projects_data = []
+    print(len(most_popular_projects_data))
     return most_popular_projects_data
 
 
-def fetch_open_issues_amount(most_popular_projects_data):
-    repo_full_name = most_popular_projects_data['full_name']
+def fetch_open_issues_amount(project_data):
+    repo_full_name = project_data['full_name']
     search_issues_url = (
         'https://api.github.com/repos/{}/issues'.format(repo_full_name)
     )
     try:
-        issue_response = requests.get(search_issues_url)
-        return len(issue_response.json())
+        raw_response = requests.get(search_issues_url)
+        open_issues_amount = len(raw_response.json())
     except requests.exceptions.RequestException:
-        return None
+        open_issues_amount = 0
+    return open_issues_amount
+
+
+def print_popular_project_info(project_data, issues_amount, counter):
+    print("""
+{}  Repo name: {}
+    Stars: {}
+    Description: {}
+    Issues amount: {}
+    Link: {}""".format(
+        counter,
+        project_data['name'],
+        project_data['stargazers_count'],
+        project_data['description'],
+        issues_amount,
+        project_data['html_url']))
 
 
 def main():
-    top_size = 2
+    top_repos_amount = 20
     week_ago_date = get_week_ago_date()
-    print(week_ago_date)
-    find_popular_projects(week_ago_date, top_size)
+    most_popular_projects_data = fetch_popular_projects(
+        week_ago_date,
+        top_repos_amount
+    )
+    if most_popular_projects_data:
+        counter = 1
+        for project_data in most_popular_projects_data:
+            project_issues_amount = fetch_open_issues_amount(project_data)
+            print_popular_project_info(
+                project_data,
+                project_issues_amount,
+                counter
+           )
+            counter += 1
+    else:
+        print('Request to GitHub API failed.')
 
 
 if __name__ == '__main__':
